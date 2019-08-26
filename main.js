689999f219e5c20f95da43e006e4a225fe285f39
jota <jotaca03@gmail.com>
2019-08-26 06:11:07 -0300

    design responsivo adicionado e validacao de formulario concluida

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
-console.log('it works')
+function submit() {
+  var nameInput = document.getElementById('name')
+  var emailInput = document.getElementById('email')
+  var messageInput = document.getElementById('message')
+  
+  var nameValue = nameInput.nodeValue()
 
-$(document).ready(function () {
-  $('.submit').click(function (event) {
-    event.preventDefault()
-    console.log('clicked botton')
-
-    var name = $('#name').val()
-    var email = $('#email').val()
-    var message = $('#message').val()
-    var statusElm = $('.status')
-    statusElm.empty()
-
-    if(name.lenght > 2) {
-      statusElm.append('<div>Name is valid</div>')
-    } else {
-      event.preventDefault()
-      statusElm.append('<div>Name is not valid</div>')
-    }
-    if(email.lenght > 5 && email.includes('@') && email.includes('.')) {
-      statusElm.append('<div>Email is valid</div>')
-    } else {
-      event.preventDefault()
-      statusElm.append('<div>Email is not valid</div>')
-    }
-    if(message.lenght > 20) {
-       statusElm.append('<div>Message is valid</div>')
-    } else {
-      event.preventDefault()
-      statusElm.append('<div>Message is not valid</div>')
-    }
-  })
-})
+  console.log(nameValue.lenght > 3)
+  
+}
